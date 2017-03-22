089fa6599dd690a09872213fe8ce95571bf07457
MANPREET <mondair.manpreet@gmail.com>
2017-03-21 23:12:24 -0400

    made jplugin

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+ $(".datepicker").flatpickr();
+  $(".bodyimage").dense();
+
+  $('body').shards(col1,col2,highlight,levels,complexity,lightness,alpha,fullscreen);
+
+});
+
